953630ea54b3443e5f196521d283e3fce58bc291
nilay <nilay.patel@hmcts.net>
2023-09-18 15:16:53 +0100

    RIA-5679 fixing mutations

--- stryker.node.conf.js
+++ stryker.node.conf.js
@@ -1,25 +1,20 @@
 require('dotenv-extended').load({ path: 'api/.env.defaults' });
-module.exports = function (config) {
-    config.set({
-        // fileLogLevel: 'trace',
-        // logLevel: 'trace',
-        mutate: ["app/**/*.ts", "!app/test/**/*.ts"],
-        mutator: 'typescript',
-        transpilers: [
-            'typescript'
-        ],
-        testFramework: "mocha",
-        testRunner: "mocha",
-        reporters: ["clear-text", "progress", "html"],
-        tsconfigFile: 'tsconfig.json',
-        mochaOptions: {
-            spec: [ "dist/out-tsc/api/{,!(test)/**/}*.spec.js" ],
-            // timeout: 5000
-        },
-        htmlReporter: {
-            baseDir: 'reports/tests/mutation/node/'
-        },
-        concurrency: 2
-
-    });
+module.exports = {
+    // fileLogLevel: 'trace',
+    // logLevel: 'trace',
+    mutate: ["app/**/*.ts", "!app/test/**/*.ts"],
+    transpilers: [
+        'typescript'
+    ],
+    testRunner: "mocha",
+    reporters: ["clear-text", "progress", "html"],
+    tsconfigFile: 'tsconfig.json',
+    mochaOptions: {
+        spec: [ "dist/out-tsc/api/{,!(test)/**/}*.spec.js" ],
+        // timeout: 5000
+    },
+    htmlReporter: {
+        baseDir: 'reports/tests/mutation/node/'
+    },
+    concurrency: 2
 }
