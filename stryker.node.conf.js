87b36ca2e613862f67dd98fb8bad30bad8705a26
nilay <nilay.patel@hmcts.net>
2023-09-18 10:17:28 +0100

    RIA-5679 security fix

--- stryker.node.conf.js
+++ stryker.node.conf.js
@@ -1,4 +1,4 @@
-require('dotenv-extended').load({ path: 'api/.env.defaults' });
+@type {import('dotenv-extended').load({ path: 'api/.env.defaults' })}
 module.exports = function (config) {
     config.set({
         // fileLogLevel: 'trace',
