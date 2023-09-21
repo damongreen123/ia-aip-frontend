9174c0565289d62914b6464c1d0de832d4e238be
nilay <nilay.patel@hmcts.net>
2023-09-21 16:34:41 +0100

    RIA-5679 fixing security

--- codecept.e2e.ts
+++ codecept.e2e.ts
@@ -1,13 +1,16 @@
 const config = require('config');
 const process = require("process");
-
+import { bootstrap as ourBootStrap, teardown as ourTeardown } from './test/functional/bootstrap';
 
 exports.config = {
   name: 'codecept',
   output: './functional-output/e2e/reports/',
   //teardown: './test/functional/bootstrap.ts',
+  bootstrap: async () => {
+    await ourBootStrap();
+  },
   teardown: async () => {
-     process.exit();
+    await ourTeardown();
   },
   helpers: {
     Puppeteer: {
