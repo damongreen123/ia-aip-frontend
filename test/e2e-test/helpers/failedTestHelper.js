ec8a4497985979fe70f012aeec2a6ac667ca94e4
nilay <nilay.patel@hmcts.net>
2023-10-04 13:46:33 +0100

    DIAC-48 fixes?

--- test/e2e-test/helpers/failedTestHelper.js
+++ test/e2e-test/helpers/failedTestHelper.js
@@ -1,10 +1,28 @@
 const Helper = require('@codeceptjs/helper');
-const { event } = require('codeceptjs');
+const { event, output } = require('codeceptjs');
 const process = require('process');
 
 class FailedTest extends Helper {
   _failed() {
     process.exit(1);
+  }
+  async _finishTest() {
+    console.log('finished all tests wooooo')
+  }
+  async _beforeStep() {
+    const helper = this.helpers['Puppeteer'];
+    output.log('Checking flakiness');
+    try {
+      for (let i = 0; i < 10; i++) {
+        await helper.see('Sign out');
+        await helper.see('Sorry, there is a problem with this service');
+        await helper.refreshPage();
+        output.error('Saw flakey problem with service');
+        await helper.wait(5);
+      }
+    } catch (err) {
+      // do nothing
+    }
   }
 }
 
--- test/e2e-test/helpers/navigationHelper.ts
+++ test/e2e-test/helpers/navigationHelper.ts
@@ -4,20 +4,6 @@ const Helper = require('@codeceptjs/helper');
 const output = require('codeceptjs').output;
 
 class NavigationHelper extends Helper {
-  async _beforeStep() {
-    const helper = this.helpers['Puppeteer']; // Or change to another Helper
-    try {
-      for (let i = 0; i < 10; i++) {
-        await helper.see('Sign out');
-        await helper.see('Sorry, there is a problem with this service');
-        await helper.refreshPage();
-        output.error('Saw flakey problem with service');
-        await helper.wait(5);
-      }
-    } catch (err) {
-      // do nothing
-    }
-  }
   async checkIfLogInIsSuccessful(timeout) {
     const helper = this.helpers['Puppeteer']; // Or change to another Helper
     try {
