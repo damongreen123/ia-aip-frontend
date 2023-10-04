3eb18f5fc57d4f20cf4c6277b9e61cea2dcd9e3e
nilay <nilay.patel@hmcts.net>
2023-10-04 13:32:36 +0100

    DIAC-48 fixes?

--- test/e2e-test/helpers/navigationHelper.ts
+++ test/e2e-test/helpers/navigationHelper.ts
@@ -7,7 +7,8 @@ class NavigationHelper extends Helper {
   async _beforeStep() {
     const helper = this.helpers['Puppeteer']; // Or change to another Helper
     try {
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < 10; i++) {
+        await helper.see('Sign out');
         await helper.see('Sorry, there is a problem with this service');
         await helper.refreshPage();
         output.error('Saw flakey problem with service');
@@ -17,31 +18,13 @@ class NavigationHelper extends Helper {
       // do nothing
     }
   }
-  async checkIfSuccessfulLoad() {
-    const helper = this.helpers['Puppeteer']; // Or change to another Helper
-    try {
-      await helper.see('Sorry, there is a problem with this service');
-      return false;
-    } catch (err) {
-      // Do nothing
-    }
-    return true;
-  }
   async checkIfLogInIsSuccessful(timeout) {
     const helper = this.helpers['Puppeteer']; // Or change to another Helper
     try {
       await helper.wait(timeout);
       assert.ok(helper.page.url().includes('appeal-overview'));
-      for (let i = 0; i < 3; i++) {
-        let success = await this.checkIfSuccessfulLoad();
-        if (success === true) {
-          return true;
-        } else {
-          await helper.refreshPage();
-          await helper.wait(2);
-        }
-      }
-      return false;
+      await helper.see('Sign out');
+      return true;
     } catch (err) {
       return false;
     }
