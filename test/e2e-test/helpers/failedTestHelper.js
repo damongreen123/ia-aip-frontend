692f04ba8bf2f20270658ddfb19acdcd75bfe8b4
nilay <nilay.patel@hmcts.net>
2023-10-04 14:06:30 +0100

    DIAC-48 fixes?

--- test/e2e-test/helpers/failedTestHelper.js
+++ test/e2e-test/helpers/failedTestHelper.js
@@ -1,18 +1,19 @@
 const Helper = require('@codeceptjs/helper');
 const { event, output } = require('codeceptjs');
 const process = require('process');
+const assert = require('assert');
 
 class FailedTest extends Helper {
   _failed() {
     process.exit(1);
   }
-  async _finishTest() {
-    console.log('finished all tests wooooo')
-  }
   async _beforeStep() {
     const helper = this.helpers['Puppeteer'];
     output.log('Checking flakiness');
+    const unwantedStrings = ['idam', 'start-appeal', 'eligibility'];
+    const isNotContainingUnwantedString = string => !helper.page.url().includes(string);
     try {
+      assert.ok(unwantedStrings.every(isNotContainingUnwantedString));
       for (let i = 0; i < 10; i++) {
         await helper.waitForText('Sign out', 5);
         await helper.see('Sorry, there is a problem with this service');
