620c916b7ae1196456da79809682037301a77b4e
nilay <nilay.patel@hmcts.net>
2023-10-04 15:35:39 +0100

    DIAC-48 fixes?

--- test/e2e-test/helpers/failedTestHelper.js
+++ test/e2e-test/helpers/failedTestHelper.js
@@ -9,22 +9,24 @@ class FailedTest extends Helper {
   }
   async _beforeStep() {
     const helper = this.helpers['Puppeteer'];
-    output.log('Checking flakiness');
+    await output.log('Checking flakiness');
     const unwantedStrings = ['idam', 'start-appeal', 'eligibility'];
     const url = await helper.page.url();
     const isNotContainingUnwantedString = string => !url.includes(string);
     try {
-      await assert.ok(unwantedStrings.every(isNotContainingUnwantedString));
+      await output.log(isNotContainingUnwantedString)
+      assert.ok(await unwantedStrings.every(isNotContainingUnwantedString));
       for (let i = 0; i < 10; i++) {
+        await output.log('waiting for sign out text');
         await helper.waitForText('Sign out', 5);
         await helper.see('Sorry, there is a problem with this service');
-        output.log('Saw flakey problem with service');
+        await output.log('Saw flakey problem with service');
         await helper.page.reload();
-        output.log('Reloaded page');
+        await output.log('Reloaded page');
       }
     } catch (err) {
-      output.log('Found no flakiness');
-      output.log(err);
+      await output.log('Found no flakiness');
+      await output.log(err);
     }
   }
 }
