5449c58e160dcaa55f68fe41068544b52d256f3d
nilay <nilay.patel@hmcts.net>
2023-10-06 12:38:25 +0100

    testing LR to AiP NoC test upgrade

--- test/e2e-test/pages/legal-rep-create-case/legal-rep-create-case.ts
+++ test/e2e-test/pages/legal-rep-create-case/legal-rep-create-case.ts
@@ -118,7 +118,7 @@ module.exports = {
       await I.click('Continue');
       await I.waitForText('Submit', 60);
       await I.click('Submit');
-      await I.waitForText('Case list', 60);
+      await I.waitForText('Hide Filter', 60);
     });
   }
 };
